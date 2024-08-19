90bad249bf14521f2e21245f06d32b38a310c90a
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-19 10:41:15 +0530

    TMA

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+app.use(bodyParser.json());
+
+app.use('/tasks', require('./routes/tasks'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
